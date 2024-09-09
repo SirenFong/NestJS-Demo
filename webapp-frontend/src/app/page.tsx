14a7b244d7dff2a4d07655464b157f7b28132969
Hakate <audition250601@gmail.com>
2024-09-09 13:14:08 +0700

    First

---
+++ webapp-frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      Hello Word
+      <div>
+        <Button type="primary">Add New</Button>
+      </div>
+    </div>
+  );
+}
